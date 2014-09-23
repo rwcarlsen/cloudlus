2fb74038ef01511679c53a2739adb5970aec27d2
Robert Carlsen <rwcarlsen@gmail.com>
2014-09-23 16:21:21 -0500

    added example for how to run simulation param sweeps

---
+++ cmd/sweep/sweep.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+n=1
+jobs=""
+
+echo "generating job files"
+while read line; do
+    job=sweepjob-$n.json
+    jobs="$jobs $job"
+    cycdriver -gen $line > $job
+    ((n++))
+done <$1
+echo "running jobs"
+
+results=$(cloudlus submit sweepjob-*.json)
+
+echo "extracting results"
+
+for job in $results; do
+    dir=$(cloudlus unpack $job)
+    out=$(cat $dir/out.txt)
+    echo "$dir $out"
+done
+
