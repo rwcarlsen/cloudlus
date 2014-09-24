32d2668eab6cb19661c84fe49fb5353a674835ae
Robert Carlsen <rwcarlsen@gmail.com>
2014-09-24 09:39:53 -0500

    added cmd for submitting condor jobs

---
+++ cmd/condorbots/condorbots.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+
+	"code.google.com/p/go.crypto/ssh"
+	"code.google.com/p/go.crypto/ssh/agent"
+)
+
+var (
+	n       = flag.Int("n", 10, "number of bots to deploy")
+	keyfile = flag.String("keyfile", filepath.Join(os.Getenv("HOME"), ".ssh/id_rsa"), "path to ssh private key file")
+	user    = flag.String("user", "rcarlsen", "condor (and via node) ssh username")
+	via     = flag.String("via", "best-tux.cae.wisc.edu:22", "intermediate server URI (if needed)")
+	dst     = flag.String("dst", "submit-3.chtc.wisc.edu:22", "condor submit node URI")
+	submit  = flag.String("submit", "", "submit file to send")
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+	flag.Usage = func() {
+		fmt.Println("Usage: condor [FILE...]")
+		fmt.Println("Copy listed files to condor submit node and possibly submit a job.\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if *via == "" && *dst == "" {
+		submitlocal()
+	} else {
+		submitssh()
+	}
+}
+
+func submitlocal() {
+	panic("not implemented")
+}
+
+func submitssh() {
+	// use ssh agent
+	agentconn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer agentconn.Close()
+	ag := agent.NewClient(agentconn)
+	config := &ssh.ClientConfig{
+		User: *user,
+		Auth: []ssh.AuthMethod{ssh.PublicKeysCallback(ag.Signers)},
+	}
+
+	// connect to server (with optional hops)
+	var client *ssh.Client
+	if *via != "" {
+		client, err = ssh.Dial("tcp", *via, config)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if client != nil && *dst != "" {
+		client, err = Hop(client, *dst, config)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fnames := flag.Args()
+	if *submit != "" {
+		fnames = append(fnames, *submit)
+	}
+
+	// copy files
+	for _, fname := range fnames {
+		fmt.Printf("copying %v\n", fname)
+		f, err := os.Open(fname)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = copyFile(client, f, fname)
+		if err != nil {
+			log.Fatal(err)
+		}
+		f.Close()
+	}
+
+	if *submit != "" {
+		out, err := combined(client, "condor_submit "+*submit)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", out)
+	}
+}
+
+func copyFile(c *ssh.Client, r io.Reader, path string) error {
+	s, err := c.NewSession()
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	w, err := s.StdinPipe()
+	if err != nil {
+		return err
+	}
+
+	s.Start("tee " + path)
+
+	_, err = io.Copy(w, r)
+	if err != nil {
+		return err
+	}
+	w.Close()
+
+	return s.Wait()
+}
+
+func combined(c *ssh.Client, cmd string) ([]byte, error) {
+	s, err := c.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	defer s.Close()
+
+	return s.CombinedOutput(cmd)
+}
+
+func Hop(through *ssh.Client, toaddr string, c *ssh.ClientConfig) (*ssh.Client, error) {
+	hopconn, err := through.Dial("tcp", toaddr)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, chans, reqs, err := ssh.NewClientConn(hopconn, toaddr, c)
+	if err != nil {
+		return nil, err
+	}
+
+	return ssh.NewClient(conn, chans, reqs), nil
+}
