d8d4e104153927b01077517103564e61f0a0a25a
Robert Carlsen <rwcarlsen@gmail.com>
2014-09-27 12:58:51 -0500

    update init.sh - assumes cyc-cde.tar.gz package has cycdriver in it

--- example/sweep/init.sh
+++ example/sweep/init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 tar -xzf cyc-cde.tar.gz
+
+echo '#!/bin/bash' > cyclus
+echo "$PWD/cyc-cde/cde-exec /home/robert/cyc/install/bin/cyclus \$@" >> cyclus
+chmod a+x cyclus
+
+echo '#!/bin/bash' > cycdriver
+echo "$PWD/cyc-cde/cde-exec /home/robert/gopath/bin/cycdriver \$@" >> cycdriver
 chmod a+x cycdriver
 
