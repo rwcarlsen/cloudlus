cb74b785d9b887dd2b5755d9fc92c26e056d9f3e
Robert Carlsen <rwcarlsen@gmail.com>
2015-07-02 11:15:07 -0500

    round trip vars -> deploy sched -> vars -> ...

--- cmd/cycobj/main.go
+++ cmd/cycobj/main.go
@@ -4,9 +4,11 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"text/tabwriter"
 
 	"github.com/rwcarlsen/cloudlus/Godeps/_workspace/src/github.com/rwcarlsen/cyan/post"
@@ -15,9 +17,10 @@ import (
 )
 
 var (
-	transform = flag.Bool("transform", false, "print the deployment schedule form of the passed variables")
-	scenfile  = flag.String("scen", "scenario.json", "file containing problem scenification")
-	db        = flag.String("db", "", "database file to calculate objective for")
+	transform   = flag.Bool("transform", false, "print the deployment schedule form of the passed variables")
+	untransform = flag.Bool("untransform", false, "print the variables form of the passed build schedule")
+	scenfile    = flag.String("scen", "scenario.json", "file containing problem scenification")
+	db          = flag.String("db", "", "database file to calculate objective for")
 )
 
 // with no flags specified, compute and run simulation
@@ -30,33 +33,61 @@ func main() {
 		check(err)
 	}
 
-	scen := &scen.Scenario{}
-	err = scen.Load(*scenfile)
+	scn := &scen.Scenario{}
+	err = scn.Load(*scenfile)
 	check(err)
 
 	if flag.NArg() > 0 {
-		_, err = scen.TransformVars(params)
+		_, err = scn.TransformVars(params)
 		check(err)
 	}
 
 	if *transform {
 		tw := tabwriter.NewWriter(os.Stdout, 4, 4, 1, ' ', 0)
 		fmt.Fprint(tw, "Prototype\tBuildTime\tNumber\n")
-		for _, b := range scen.Builds {
+		for _, b := range scn.Builds {
 			fmt.Fprintf(tw, "%v\t%v\t%v\n", b.Proto, b.Time, b.N)
 		}
 		tw.Flush()
+	} else if *untransform {
+		data, err := ioutil.ReadAll(os.Stdin)
+		check(err)
+		s := string(data)
+		lines := strings.Split(s, "\n")
+		builds := []scen.Build{}
+		for _, l := range lines {
+			l = strings.TrimSpace(l)
+			fields := strings.Fields(l)
+			if len(l) == 0 {
+				continue
+			} else if fields[1] == "BuildTime" {
+				continue
+			}
+			proto := fields[0]
+			t, err := strconv.Atoi(fields[1])
+			check(err)
+			n, err := strconv.Atoi(fields[2])
+			check(err)
+			builds = append(builds, scen.Build{Proto: proto, Time: t, N: n})
+		}
+		scn.Builds = builds
+		vars, err := scn.TransformSched()
+		check(err)
+
+		for _, val := range vars {
+			fmt.Printf("%v\n", val)
+		}
 	} else if *db != "" {
 		dbh, err := sql.Open("sqlite3", *db)
 		check(err)
 		defer dbh.Close()
 		simids, err := post.Process(dbh)
-		val, err := scen.CalcObjective(*db, simids[0])
+		val, err := scn.CalcObjective(*db, simids[0])
 		check(err)
 		fmt.Println(val)
 	} else {
-		dbfile, simid, err := scen.Run(nil, nil)
-		val, err := scen.CalcObjective(dbfile, simid)
+		dbfile, simid, err := scn.Run(nil, nil)
+		val, err := scn.CalcObjective(dbfile, simid)
 		check(err)
 		fmt.Println(val)
 	}
